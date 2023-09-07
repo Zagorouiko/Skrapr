e5d6fd8a32caa9761098a202d3d56d9a08640be0
Leon Zagorouiko <Zagorouiko@gmail.com>
2023-09-07 14:00:04 -0700

    added error handling

--- source/routes/reviews.ts
+++ source/routes/reviews.ts
@@ -2,7 +2,22 @@ import express from "express";
 import controller from "../controllers/reviews";
 const router = express.Router();
 
-router.get("/reviews/Android/:app", controller.getAndroidReviews);
-router.get("/reviews/iOS/:app", controller.getiOSReviews);
+router.get("/reviews/Android/:app", async (req, res, next) => {
+    try {
+      const androidReviews = await controller.getAndroidReviews(req, res, next);
+      res.json(androidReviews);
+    } catch (error) {
+      next(error); 
+    }
+  });
+
+router.get("/reviews/iOS/:app", async (req, res, next) => {
+    try {
+      const iOSReviews = await controller.getiOSReviews(req, res, next);
+      res.json(iOSReviews);
+    } catch (error) {
+      next(error);
+    }
+  });
 
 export = router;
